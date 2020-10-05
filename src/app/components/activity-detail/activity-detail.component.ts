0551b3135c057914be228c26d9648998f23cf9b6
taniagarcia89 <taniag89@gmail.com>
2020-10-05 09:13:09 +0200

    favorites

--- src/app/components/activity-detail/activity-detail.component.ts
+++ src/app/components/activity-detail/activity-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,  Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
+import { Router } from '@angular/router';
 import { GlobalService } from '../../Services/global.service';
 
 import { ActivityService } from '../../Services/activity.service';
@@ -20,7 +20,8 @@ export class ActivityDetailComponent implements OnInit {
 
   user: User;
   users: User[];
-  constructor( private userService:UserService, private _globalService: GlobalService )
+  
+  constructor( private userService:UserService, private _globalService: GlobalService, private router: Router )
   {
     this.user = this._globalService.globalVar;
   }
@@ -45,6 +46,21 @@ export class ActivityDetailComponent implements OnInit {
     }
   }
 
+  favorited() {
+    const saved = JSON.parse(localStorage.getItem('favorites'));
+
+    if (saved !== null) {
+
+      for (let i = 0; i < saved.length; i++) {
+        if (saved[i].id === this.activity.id) {
+          return true;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+
   getUsers(): void{
     this.userService.getUsers()
       .subscribe(users => this.users = users);
@@ -53,18 +69,42 @@ export class ActivityDetailComponent implements OnInit {
   favorite() {
 
     const saved = JSON.parse(localStorage.getItem('favorites'));
-console.log(this.activity, saved)
+    console.log(this.activity, saved)
+
     if (saved !== null) {
-      const fav = [];
-      fav.push(this.activity);
-      fav.push(saved);
-      localStorage.setItem('favorites', JSON.stringify(fav));
+
+      saved.push(this.activity);
+      localStorage.setItem('favorites', JSON.stringify(saved));
     } else {
       const fav = [];
       fav.push(this.activity);
       console.log(fav)
       localStorage.setItem('favorites', JSON.stringify(fav));
     }
+  }
+
+  unfavorite() {
+
+    const saved = JSON.parse(localStorage.getItem('favorites'));
+
+    for (let i = 0; i < saved.length; i++){
+          if (saved[i].id === this.activity.id) {
+            saved.splice(i, 1);
+            }
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(saved));
+
+    const url = window.location.pathname;
+
+    if (url === '/favorites') {
+      this.reload(url);
+    }
+  }
+
+  async reload(url: string): Promise<boolean> {
+    await this.router.navigateByUrl('/login', { skipLocationChange: true });
+    return this.router.navigateByUrl(url);
   }
 
   signUp(activity) {
